0a78c2e43754f3d24edcdd110ea4d14a23616495
Guest User <Guest@Epicodus-5E.local>
2018-04-24 16:37:17 -0700

    make prack

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Ticket from "./Ticket"
+function TicketList(){
+  return (
+    <Ticket   location="3A"
+      names="Thato and Haley"
+      issue="Firebase Sucks! So I ate my own feces!" />
+  );
+}
+
+export default TicketList;
